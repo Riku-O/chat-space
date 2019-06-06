846568ba5a97abe0ab8d74b5b9a82231c568aca8
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2019-06-06 18:34:25 +0900

    fix 自動更新

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    # binding.pry
     @group = Group.find(params[:group_id])
     @messages = @group.messages.where('id > ?', params[:id])
     respond_to do |format|
