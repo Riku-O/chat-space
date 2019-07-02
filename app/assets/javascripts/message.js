71410851fa0123a2a7eaff4942b5e0f661f6fb58
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2019-07-02 20:19:21 +0900

    fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,6 +35,7 @@ $(function () {
             contentType: false
           })
           .done(function (data) {
+            console.log(data);
             var html = buildHTML(data);
             $('.messages').append(html);
             $('.messages').animate({
